a97ffbb7dea4a2eb410da9e95c62b67315f49e57
Michel Metran <michelmetran@gmail.com>
2023-02-22 20:28:14 -0300

    Ajustes

--- scripts/paths.py
+++ scripts/paths.py
@@ -1,28 +1,38 @@
-import os
+"""
+Pastas do projeto
+nov.22
+"""
 
 
-data_path = os.path.join('..', 'data')
-os.makedirs(data_path, exist_ok=True)
-
-input_path = os.path.join(data_path, 'input')
-os.makedirs(input_path, exist_ok=True)
-
-input_path_tab = os.path.join(input_path, 'tabs')
-os.makedirs(input_path_tab, exist_ok=True)
+from pathlib import Path
 
 
+project_path = Path(__file__).parents[1]
+
+# Data
+data_path = project_path / 'data'
+data_path.mkdir(exist_ok=True)
+
+# Input
+input_path = data_path / 'input'
+input_path.mkdir(exist_ok=True)
+
+input_path_tab = input_path / 'tabs'
+input_path_tab.mkdir(exist_ok=True)
+
+# Output
+output_path = data_path / 'output'
+output_path.mkdir(exist_ok=True)
+
+output_path_tabs = output_path / 'tabs'
+output_path_tabs.mkdir(exist_ok=True)
+
+output_path_geo = output_path / 'geo'
+output_path_geo.mkdir(exist_ok=True)
+
+output_path_maps = output_path / 'maps'
+output_path_maps.mkdir(exist_ok=True)
 
 
-output_path = os.path.join(data_path, 'output')
-os.makedirs(output_path, exist_ok=True)
-
-output_path_tabs = os.path.join(output_path, 'tabs')
-os.makedirs(output_path_tabs, exist_ok=True)
-
-output_path_geo = os.path.join(output_path, 'geo')
-os.makedirs(output_path_geo, exist_ok=True)
-
-output_path_maps = os.path.join(output_path, 'maps')
-os.makedirs(output_path_maps, exist_ok=True)
-
-
+if __name__ == '__main__':
+    print(project_path)
